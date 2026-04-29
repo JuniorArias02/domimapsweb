06a49e6387e671cf3a697c3fb1f6f9bbf85388cd
Junior Arias <junior.arias02yt@gmail.com>
2026-04-29 14:29:37 -0500

    correcion y cambiod enombre

--- src/constants/navegacion.js
+++ src/constants/navegacion.js
@@ -4,7 +4,7 @@ export const itemsNavegacion = [
   { icono: Home, etiqueta: 'Panel', ruta: '/panel' },
   { icono: Map, etiqueta: 'Mapa', ruta: '/mapa' },
   { icono: Users, etiqueta: 'Pacientes', ruta: '/pacientes' },
-  { icono: Calendar, etiqueta: 'Agenda', ruta: '/agenda' }
+  { icono: Calendar, etiqueta: 'Atenciones', ruta: '/agenda' }
 ];
 
 export const itemsNavegacionSecundaria = [
